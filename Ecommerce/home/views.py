f9e11ee3134b221912305440e577bc6f860c08b4
UmeshReddyKathula <kathulaumesh071998@gmail.com>
2022-08-02 23:03:33 +0530

    offcanvas menu area created

--- Ecommerce/home/views.py
+++ Ecommerce/home/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def homeapp(request):
-    return HttpResponse('i am umesh reddy')
+    return render(request,'home.html')
